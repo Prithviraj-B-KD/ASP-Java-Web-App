55a21d5470059ce5b9741dd5db9fb14cbdd49214
prithvi206 <prithviraj2062002@gmail.com>
2024-01-13 11:37:20 +0530

    Java web app

---
+++ src/main/java/com/kdu/assignment2/DemoProjectForSpringBootApplication.java
@@ -0,0 +1,21 @@
+package com.kdu.assignment2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoProjectForSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectForSpringBootApplication.class, args);
+	}
+	@RestController
+	class HelloController {
+		@GetMapping("/")
+		public String index() {
+			return "Hello, Prithvi!";
+		}
+	}
+}
